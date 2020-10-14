94760a080c69801934db354504ccb812c0a9dbdb
Marcelo Carvalho <marcelo.scientist@hotmail.com>
2020-10-13 22:07:27 -0300

    Fixed game over problem and finished the basics of the game

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
+let position = 0;
 
 let isJumping = false;
 function handleKeyUp(event) {
@@ -11,7 +12,6 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0;
     isJumping = true;
     let upInterval = setInterval(() => {
         if (position >= 150) {
@@ -36,9 +36,27 @@ function jump() {
 function createCactus() {
     const cactus = document.createElement('div');
     let cactusPos = 1000;
+    let randomTime = Math.random() * 6000;
+
     cactus.classList.add('cactus');
     cactus.style.left = cactusPos + 'px';
     background.appendChild(cactus);
+
+    let leftInterval = setInterval(()=> {
+        if(cactusPos < -60) {
+            clearInterval(leftInterval)
+            background.removeChild(cactus)
+        } else if(cactusPos > 0 && cactusPos < 60 && position < 60) {
+            clearInterval(leftInterval)
+            document.body.innerHTML = '<h1>Fim de Jogo</h1>'
+        } 
+        else {
+            cactusPos -= 10;
+            cactus.style.left = cactusPos + 'px';          
+        }
+    }, 20)
+
+    setTimeout(createCactus, randomTime)
 }
 
 createCactus();
